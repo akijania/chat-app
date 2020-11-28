6ef5f04352cfbe36e8fbe2fc0f1ba5d1cc545ac1
Anna Kijania <kijania.anna@gmail.com>
2020-11-28 14:22:01 +0100

    Add WebSockets

--- client/app.js
+++ client/app.js
@@ -5,6 +5,8 @@ const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 let userName = '';
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content));
 
 const login = function (event) {
   event.preventDefault();
@@ -14,6 +16,12 @@ const login = function (event) {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('join', { name: userName, id: socket.id });
+    addMessage('Chat Bot', `${userName} has joined the conversation!`);
+    socket.emit('message', {
+      author: 'Chat Bot',
+      content: `${userName} has joined the conversation!`,
+    });
   }
 };
 
@@ -26,6 +34,9 @@ const addMessage = function (author, content) {
   if (author === userName) {
     message.classList.add('message--self');
   }
+  if (author === 'Chat Bot') {
+    message.classList.add('message--bot');
+  }
   message.innerHTML = `
   <h3 class="message__author">${userName === author ? 'You' : author}</h3>
   <div class="message__content">
@@ -37,10 +48,13 @@ const addMessage = function (author, content) {
 
 const sendMessage = function (event) {
   event.preventDefault();
-  if (!messageContentInput.value.length) {
+
+  let messageContent = messageContentInput.value;
+  if (!messageContent.length) {
     alert('Please add your message');
   } else {
-    addMessage(userName, messageContentInput.value);
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent });
     messageContentInput.value = '';
   }
 };
--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
-const messages = [];
+let messages = [];
+let users = [];
 app.use(express.static(path.join(__dirname, '/client')));
-app.get('*', (req, res) => {
-    res.render(path.join(__dirname, '/client/index.html'));
-  });
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
   });
-  
+  socket.on('join', (user) => {
+    console.log('Oh, we have a new user ' + socket.id);
+    users.push(user);
+    console.log(users);
+  });
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+    const user = users.filter((user) => user.id == socket.id);
+    const index = users.indexOf(user);
+    socket.broadcast.emit('message', {
+      author: 'Chat Bot',
+      content: `${user[0].name} has left the conversation... :(`,
+    });
+    users.splice(index, 1);
+  });
+  console.log("I've added a listener on message and disconnect events \n");
+});
